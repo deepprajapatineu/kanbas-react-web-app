a02fa17768a3ed777addec52875a7eb1fa75fbd9
Deep Prajapati <prajapati.de@northeastern.edu>
2023-10-19 23:15:05 -0400

    change in nav bar

--- src/Kanbas/Dashboard/Components/CourseList.js
+++ src/Kanbas/Dashboard/Components/CourseList.js
@@ -6,7 +6,7 @@ function CourseList({ courses }) {
   return (
     <div className="d-flex flex-row flex-wrap ms-4">
       <div className="col-12 fs-4">
-        <span>Published Courses (24)</span>
+        <span>Published Courses (8)</span>
       </div>
       <hr className="w-100" />
       {courses.map((course) => (
